70b3bfcd1ea61fcfc7b4d09dcff7654478f000eb
Ulysse Carion <ulysse@segment.com>
2021-02-06 10:16:52 -0800

    Add documentation, update release process, polish interface (#2)
    * Update to GitHub Release-based workflow
    
    * Move to cli.yaml + refactor to jtd@0.3
    
    * Add README
    
    * cargo fmt
    
    * Bump to v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,83 @@
-use anyhow::{format_err, Context, Result};
-use clap::{crate_version, App, AppSettings, Arg};
-use jtd::{Schema, SerdeSchema, Validator};
+use anyhow::{Context, Result};
+use clap::{crate_version, load_yaml, App, AppSettings};
+use jtd::{Schema, ValidateOptions};
 use serde::Serialize;
 use serde_json::Deserializer;
-use std::convert::TryInto;
+use std::borrow::Cow;
 use std::fs::File;
 use std::io::{stdin, BufReader, Read};
 use std::process::exit;
 
 fn main() -> Result<()> {
-    let matches = App::new("jtd-validate")
-        .version(crate_version!())
-        .about("Validate JSON data against a JSON Typedef schema")
+    let cli_yaml = load_yaml!("cli.yaml");
+    let matches = App::from(cli_yaml)
         .setting(AppSettings::ColoredHelp)
-        .arg(
-            Arg::with_name("quiet")
-                .help("Disable outputting validation errors.")
-                .long("quiet")
-                .short("q")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("max-depth")
-                .help("How many refs to follow recursively before erroring. By default, refs are followed until stack overflow.")
-                .long("max-depth")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("max-errors")
-                .help("Maximum number of errors to look for in each instance. By default, all validation errors are returned. Specifiying --quiet implies --max-errors=1.")
-                .long("max-errors")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("schema")
-                .help("File containing the schema to validate against")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("instances")
-                .help("Read data to validate (\"instances\") from this file, instead of STDIN"),
-        )
+        .version(crate_version!())
         .get_matches();
 
-    // Parse flags as soon as possible, to give users errors if they mistyped
-    // something.
     let quiet = matches.is_present("quiet");
-    let max_depth: Option<usize> = match matches.value_of("max-depth") {
-        Some(s) => Some(
-            s.parse()
-                .with_context(|| format!("Failed to parse max depth: {}", s))?,
-        ),
-        None => None,
-    };
 
-    let max_errors: Option<usize> = match matches.value_of("max-errors") {
-        Some(s) => Some(
-            s.parse()
-                .with_context(|| format!("Failed to parse max errors: {}", s))?,
-        ),
-        None => {
-            // If the user has specified --quiet, then all they care about is
-            // whether there are any validation errors at all. So there's no
-            // point in looking for more than one error.
-            if quiet {
-                Some(1)
-            } else {
-                None
-            }
-        }
-    };
-
-    // Open up the relevant files, so we can get I/O errors back to the user as
-    // soon as possible if the files don't exist or something.
-    let schema = BufReader::new(
-        File::open(matches.value_of("schema").unwrap())
-            .with_context(|| format!("Failed to open schema file"))?,
-    );
-
-    let instances: Box<dyn Read> = if let Some(file) = matches.value_of("instances") {
-        Box::new(BufReader::new(
-            File::open(file).with_context(|| format!("Failed to open instances file"))?,
-        ))
+    let max_depth = if let Some(s) = matches.value_of("max-depth") {
+        s.parse()
+            .with_context(|| format!("Failed to parse max depth: {}", s))?
     } else {
-        Box::new(stdin())
+        0
     };
 
-    // Parse the schema input file into a jtd::Schema.
-    let serde_schema: SerdeSchema =
-        serde_json::from_reader(schema).with_context(|| format!("Failed to parse schema"))?;
-
-    let schema: Schema = serde_schema
-        .try_into()
-        .map_err(|err| format_err!("invalid schema: {:?}", err))
-        .with_context(|| format!("Failed to load schema"))?;
-
-    schema
-        .validate()
-        .map_err(|err| format_err!("invalid schema: {:?}", err))
-        .with_context(|| format!("Failed to verify schema correctness"))?;
-
-    // Construct a validator with the user's supplied max-depth and max-errors,
-    // if any.
-    let validator = Validator {
-        max_depth,
-        max_errors,
+    let max_errors = if let Some(s) = matches.value_of("max-errors") {
+        s.parse()
+            .with_context(|| format!("Failed to parse max errors: {}", s))?
+    } else if quiet {
+        1
+    } else {
+        0
     };
 
-    // Validate each input from instances against the schema.
-    let stream = Deserializer::from_reader(instances);
+    let schema_reader = BufReader::new(match matches.value_of("schema").unwrap() {
+        "-" => Box::new(stdin()) as Box<dyn Read>,
+        file @ _ => Box::new(File::open(file)?) as Box<dyn Read>,
+    });
+
+    let schema = Schema::from_serde_schema(
+        serde_json::from_reader(schema_reader).with_context(|| "Failed to parse schema")?,
+    )
+    .with_context(|| "Malformed schema")?;
+
+    schema.validate().with_context(|| "Invalid schema")?;
+
+    let instance_reader = BufReader::new(match matches.value_of("instances").unwrap() {
+        "-" => Box::new(stdin()) as Box<dyn Read>,
+        file @ _ => Box::new(File::open(file)?) as Box<dyn Read>,
+    });
+
+    let stream = Deserializer::from_reader(instance_reader);
     for instance in stream.into_iter() {
         let instance = instance.with_context(|| format!("Failed to parse instance"))?;
 
-        let errors = validator
-            .validate(&schema, &instance)
-            .map_err(|err| format_err!("invalid schema: {:?}", err))
-            .with_context(|| format!("Failed to validate instance"))?;
+        let errors = jtd::validate(
+            &schema,
+            &instance,
+            ValidateOptions::new()
+                .with_max_depth(max_depth)
+                .with_max_errors(max_errors),
+        )
+        .with_context(|| format!("Failed to validate instance"))?;
 
         if !errors.is_empty() {
             if !quiet {
                 // These are the errors we'll output to the user, using the standard
                 // JSON Typedef error indicator format.
                 let error_indicators: Vec<_> = errors
-                    .iter()
+                    .into_iter()
                     .map(|err| ErrorIndicator {
-                        instance_path: to_json_pointer(&err.instance_path),
-                        schema_path: to_json_pointer(&err.schema_path),
+                        instance_path: to_json_pointer(err.instance_path),
+                        schema_path: to_json_pointer(err.schema_path),
                     })
                     .collect();
 
-                println!("{}", serde_json::to_string(&error_indicators).unwrap());
+                for error_indicator in error_indicators {
+                    println!("{}", serde_json::to_string(&error_indicator).unwrap());
+                }
             }
 
             exit(1);
@@ -148,7 +96,7 @@ struct ErrorIndicator {
     schema_path: String,
 }
 
-fn to_json_pointer(path: &[String]) -> String {
+fn to_json_pointer<'a>(path: Vec<Cow<'a, str>>) -> String {
     if path.is_empty() {
         "".to_owned()
     } else {
